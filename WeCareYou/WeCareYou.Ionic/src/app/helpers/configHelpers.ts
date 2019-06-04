181c90a2a28e3f32486fb779cce697daf459e6b6
Gabriel Milare <gabriel.milare@bliteti.com.br>
2019-06-04 20:06:37 -0300

    v

--- WeCareYou/WeCareYou.Ionic/src/app/helpers/configHelpers.ts
+++ WeCareYou/WeCareYou.Ionic/src/app/helpers/configHelpers.ts
@@ -1,7 +1,6 @@
 export class ConfigHelper {
     public static Url: string = '/v1/'
     // public static Url: string = 'http://localhost:3000/api/'
-    //public static Url: string = 'http://nofood-com-br.umbler.net/api/'
 
     public static storageKeys = {
         token: 'WeCareYou.token',
